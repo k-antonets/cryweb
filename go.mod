f6ec5907d462283c3b91cdea30b573057ff6d8ae
orodret <orodret15@gmail.com>
2019-08-29 14:27:29 +0300

    Add templates structure

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/lab7arriam/cryweb
 go 1.12
 
 require (
+	github.com/foolin/goview v0.2.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 
+	"github.com/foolin/goview"
+	"github.com/foolin/goview/supports/echoview"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 var (
@@ -13,6 +16,27 @@ var (
 func main() {
 	flag.Parse()
 	e := echo.New()
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Skipper: middleware.DefaultSkipper,
+		Root:    "/static",
+		Index:   "index.html",
+		HTML5:   true,
+		Browse:  false,
+	}))
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Renderer = echoview.New(goview.Config{
+		Root:      "templates",
+		Extension: ".tmpl",
+		Master:    "layouts/base",
+		Partials:  []string{"assets/js", "assets/style", "assets/login"},
+	})
+
+	e.Use(middleware.BodyLimit("400M"))
+
+	e.GET("/", IndexHndl)
+	e.GET("/login", LoginHndl)
 
 	e.Logger.Fatal(e.Start(*url))
 }

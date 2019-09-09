6db55a7aae808234eed74c5ecef89c9c5f7fb9aa
k-antonets <k.antonets@arriam.ru>
2019-09-09 13:56:04 +0300

    add support for config files

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/foolin/goview v0.2.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/spf13/viper v1.4.0
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586 // indirect
 )
--- main.go
+++ main.go
@@ -7,14 +7,20 @@ import (
 	"github.com/foolin/goview/supports/echoview"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/spf13/viper"
 )
 
 var (
-	url = flag.String("url", ":8080", "Url to listen to")
+	config = flag.String("config", "config.cfg", "path ro config file")
 )
 
 func main() {
 	flag.Parse()
+
+	viper.SetConfigName(*config)
+	viper.AddConfigPath("/run/secrets")
+	viper.AddConfigPath(".")
+
 	e := echo.New()
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
 		Skipper: middleware.DefaultSkipper,
@@ -25,6 +31,14 @@ func main() {
 	}))
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			e.Logger.Fatal("config file not found: " + err.Error())
+		} else {
+			e.Logger.Fatal("error with reading config file: " + err.Error())
+		}
+	}
 
 	e.Renderer = echoview.New(goview.Config{
 		Root:      "templates",
@@ -38,5 +52,5 @@ func main() {
 	e.GET("/", IndexHndl)
 	e.GET("/login", LoginHndl)
 
-	e.Logger.Fatal(e.Start(*url))
+	e.Logger.Fatal(e.Start(viper.GetString("url")))
 }

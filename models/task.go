a6698ba9f922bf2102cee0b367a82648138c8f97
k-antonets <k.antonets@arriam.ru>
2019-10-04 16:44:54 +0300

    add task model

---
+++ models/task.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"io/ioutil"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Task struct {
+	Id       bson.ObjectId     `json:"id" bson:"_id"`
+	Created  time.Time         `json:"created" bson:"created"`
+	Status   string            `json:"status" bson:"status"`
+	Finished time.Time         `json:"finished" bson:"finished,omitempty"`
+	Params   map[string]string `json:"params" bson:"params"`
+	WorkDir  string            `json:"work_dir" bson:"work_dir"`
+	Removed  bool              `json:"removed" bson:"removed"`
+	UserId   string            `json:"user_id" bson:"user_id"`
+	Tool     string            `json:"tool" bson:"tool"`
+}
+
+func NewTask(user, tool, tmpDir string) (*Task, error) {
+	work_dir, err := ioutil.TempDir(tmpDir, tool)
+	if err != nil {
+		return nil, err
+	}
+	return &Task{
+		Created: time.Now(),
+		Status:  "created",
+		WorkDir: work_dir,
+		Removed: false,
+		UserId:  user,
+		Tool:    tool,
+	}, nil
+}
+
+func (t *Task) ResultAvailable(user string) bool {
+	if user != t.UserId || t.Status != "finished" || t.Removed {
+		return false
+	}
+	return true
+}

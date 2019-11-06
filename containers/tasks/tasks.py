fd27ec30ab7067c2fa4fe760a1957bbde2630690
KirA <kirantonez@gmail.com>
2019-11-07 01:27:45 +0300

    process files and delete unneeded files

--- containers/tasks/tasks.py
+++ containers/tasks/tasks.py
@@ -1,9 +1,11 @@
 from celery import Celery
 import sys
+import glob
 sys.path.insert(1, '/cry_processor/')
 
 from cry_processor import Crylauncher
 from os import path
+import os
 import subprocess
 
 app = Celery("tasks", broker="redis://redis:6379/", backend="redis://redis:6379/")
@@ -14,6 +16,7 @@ app.conf.default_queue = 'cry_py'
 def cryprocess(run_mode, fi, fr, rr, meta, wd, th):
     hm, pr, ma, r, a, nu, mra, k, s, f = '', 1, '', 'do', False, '', False, 21, True, True
     od = path.join(wd, 'cry')
+    final_result_dir = path.join(wd, 'cry_processor')
     od_file = path.join(wd, 'cry_result.zip')
     fi = path.join(wd, fi)
     fr = path.join(wd, fr)
@@ -26,5 +29,12 @@ def cryprocess(run_mode, fi, fr, rr, meta, wd, th):
         meta = True
     Crylauncher.LaunchProcessor(od, fi, hm, pr, th, ma, r, a, nu, mra, k, fr, rr, meta, s, f)
 
-    subprocess.call("zip -r {0} {1}".format(od_file, od), shell=True)
+    os.mkdir(final_result_dir)
+    os.replace(glob.glob(path.join(od, 'raw_full_*'))[0], path.join(final_result_dir, 'full_toxins.fasta'))
+    os.replace(glob.glob(path.join(od, 'protein_domain_mapping_full_*'))[0], path.join(final_result_dir, 'full_toxins.bed'))
+    os.replace(path.join(od, 'logs', 'cry_processor.txt'), path.join(final_result_dir, 'summary_log.txt'))
+    os.replace(glob.glob(path.join(od, 'logs', 'diamond_matches_*'))[0], path.join(final_result_dir, 'diamond_classification.txt'))
+    subprocess.call("zip -r {0} {1}".format(od_file, final_result_dir), shell=True)
+    os.rmdir(od)
+    os.rmdir(final_result_dir)
     return wd
